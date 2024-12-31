45f0342bf01b512163a3e53409a8faabc1cfa1fe
rishadK70 <rishadmhd70@gmail.com>
2024-12-31 14:25:31 +0530

    Add files via upload

---
+++ lib/data/EletronicsData.dart
@@ -0,0 +1,33 @@
+class Eletronicsdata{
+  static List<Map<String,dynamic>>groceryProducts=[
+    {
+      'id':'1',
+      'name':'Tv',
+      'description':'Smart Tv from Samsung',
+      'price':25.49,
+      'imageurl':'https://emcor.com.ph/wp-content/uploads/2024/05/UA43DU7000GXXP.jpg'
+    },
+    {
+      'id':'2',
+      'name':'Laptop',
+      'description':'Best Laptop from Dell',
+      'price':22.99,
+      'imageurl':'https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/g-series/g16-7630/media-gallery/black/notebook-g16-7630-nt-black-gallery-1.psd?fmt=png-alpha&pscan=auto&scl=1&hei=402&wid=536&qlt=100,1&resMode=sharp2&size=536,402&chrss=full'
+    },
+    {
+      'id':'3',
+      'name':'Home Theatre',
+      'description':'Best Home Theater From jio mart',
+      'price':15.49,
+      'imageurl':'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ8gyQuj-QXG-FCqyA3DdO9AbG5YgCy8Pz0_3j1HJMpSDYBO650kcHx3ePdfXFpdiv6F1smFmPvDzZ1h8jHx460R4QeFf6V7y5m1rLINvnfcJ1lZiABn3Ar'
+    },
+    {
+      'id':'4',
+      'name':'Mobile',
+      'description':'Best Mobile from Jiomart',
+      'price':12.49,
+      'imageurl':'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSh5Is7vlJ5aFY-G5ftuoBQvvsumPSm_NCmat8sFb2stZFkaV5bQk_mZyhCLUefmCUQtRZ9P7Ed01jwM7avtWdqbh1cH44q9jYiceRcLEcI'
+    }
+    ];
+
+}
